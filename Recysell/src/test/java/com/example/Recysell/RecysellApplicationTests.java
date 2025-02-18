5fb5ae30b555eba82d28cf3d8593c9bea7fa7d7c
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2025-02-18 23:37:24 +0100

    Base de datos lanzada a PostgreSql

---
+++ Recysell/src/test/java/com/example/Recysell/RecysellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Recysell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecysellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
