2a4555d9ae0a3e4a87efbec418c589e83ec72cf7
alvarocastilla05 <castilla.caalv24@triana.salesianos.edu>
2025-02-21 13:13:14 +0100

    Continuacion asociaciones

--- Recysell/src/main/java/com/example/Recysell/categoria/model/Categoria.java
+++ Recysell/src/main/java/com/example/Recysell/categoria/model/Categoria.java
@@ -1,10 +1,11 @@
 package com.example.Recysell.categoria.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.example.Recysell.producto.model.Producto;
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Getter
@@ -21,4 +22,21 @@ public class Categoria {
     private UUID id;
 
     private String nombre;
+
+    @ManyToMany(mappedBy = "listaCategorias", fetch = FetchType.LAZY)
+    @Builder.Default
+    @ToString.Exclude
+    private Set<Producto> listaProductos = new HashSet<>();
+
+    //MÉTODOS HELPER
+
+    public void addProducto(Producto p){
+        this.listaProductos.add(p);
+        p.getListaCategorias().add(this);
+    }
+
+    public void removeProducto(Producto p){
+        this.listaProductos.remove(p);
+        p.getListaCategorias().remove(this);
+    }
 }
--- Recysell/src/main/java/com/example/Recysell/producto/model/Producto.java
+++ Recysell/src/main/java/com/example/Recysell/producto/model/Producto.java
@@ -1,9 +1,14 @@
 package com.example.Recysell.producto.model;
 
+import com.example.Recysell.categoria.model.Categoria;
 import com.example.Recysell.cliente.model.Cliente;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
 @Getter
@@ -17,7 +22,7 @@ public class Producto {
 
     @Id
     @GeneratedValue
-    private UUID id;
+    private Long id;
 
     private String nombre;
 
@@ -25,11 +30,79 @@ public class Producto {
 
     private double precio;
 
-    //Asociación con Cliente.
+    //Asociación con Cliente (Productos en venta).
     @ManyToOne
     @JoinColumn(
-            name = "cliente_id_vende",
-            foreignKey = @ForeignKey(name = "fk_producto_cliente_vende")
+            name = "cliente_id_vendedor",
+            foreignKey = @ForeignKey(name = "fk_producto_cliente_vendedor")
     )
-    private Cliente cliente;
+    private Cliente clienteVendedor;
+
+    //Asociación con Cliente (Productos donados).
+    @ManyToOne
+    @JoinColumn(
+            name = "cliente_id_donante",
+            foreignKey = @ForeignKey(name = "fk_producto_cliente_donante")
+    )
+    private Cliente clienteDonante;
+
+    //Asociación con Cliente (Favoritos).
+    @ManyToMany(mappedBy = "listaFavoritos", fetch = FetchType.LAZY)
+    @Builder.Default
+    @ToString.Exclude
+    private Set<Cliente> listaClientes = new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "tiene",
+            joinColumns = @JoinColumn(name="producto_id_categoria"),
+            inverseJoinColumns = @JoinColumn(name="categoria_id_producto"),
+            foreignKey = @ForeignKey(name = "fk_producto_categoria"),
+            inverseForeignKey = @ForeignKey(name = "fk_categoria_producto")
+    )
+    @Builder.Default
+    @ToString.Exclude
+    private Set<Categoria> listaCategorias = new HashSet<>();
+
+    //MÉTODOS HELPER
+
+        //Con Cliente (Productos en venta).
+
+        public void addCliente(Cliente c){
+            this.listaClientes.add(c);
+            c.getListaFavoritos().add(this);
+        }
+
+        public void removeCliente(Cliente c){
+            c.getListaFavoritos().remove(this);
+            this.listaClientes.remove(c);
+        }
+
+        //Con Categoria.
+
+        public void addCategoria(Categoria c){
+            this.listaCategorias.add(c);
+            c.getListaProductos().add(this);
+        }
+
+        public void removeCategoria(Categoria c){
+            c.getListaProductos().remove(this);
+            this.listaCategorias.remove(c);
+        }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Producto producto = (Producto) o;
+        return getId() != null && Objects.equals(getId(), producto.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
