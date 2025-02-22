58a6fb191ddc98a2299bba0fe35b35e9e9c4d940
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2025-02-22 13:12:37 +0100

    Listar trabajadores a medias

--- Recysell/src/main/java/com/example/Recysell/trabajador/repo/TrabajadorRepository.java
+++ Recysell/src/main/java/com/example/Recysell/trabajador/repo/TrabajadorRepository.java
@@ -1,9 +1,20 @@
 package com.example.Recysell.trabajador.repo;
 
+import com.example.Recysell.trabajador.dto.GetTrabajadorDto;
 import com.example.Recysell.trabajador.model.Trabajador;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TrabajadorRepository extends JpaRepository<Trabajador, UUID> {
+
+    @Query("""
+        SELECT new com.example.Recysell.trabajador.dto.GetTrabajadorDto(
+        t.username, t.email, t.nombre, t.apellidos, t.puesto
+        )
+        FROM Trabajador t
+    """)
+    public List<GetTrabajadorDto> findAllTrabajadorDto();
 }
