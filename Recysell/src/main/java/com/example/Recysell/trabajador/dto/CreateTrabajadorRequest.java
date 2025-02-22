42bfc9f149fdaef2bbd4fbd27a31cc421e0c1d9b
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2025-02-22 11:26:19 +0100

    Al fin solucionadoo

--- Recysell/src/main/java/com/example/Recysell/trabajador/dto/CreateTrabajadorRequest.java
+++ Recysell/src/main/java/com/example/Recysell/trabajador/dto/CreateTrabajadorRequest.java
@@ -1,15 +1,34 @@
 package com.example.Recysell.trabajador.dto;
 
-import javax.management.relation.Role;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import com.example.Recysell.user.model.UserRole;
+
 
 public record CreateTrabajadorRequest(
+        @NotBlank(message = "El username no puede estar vacío")
         String username,
+
+        @NotBlank(message = "El email no puede estar vacío")
+        @Email(message = "El email debe ser válido")
         String email,
+
+        @NotBlank(message = "La contraseña no puede estar vacía")
+        @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
         String password,
+
+        @NotBlank(message = "Debe verificar la contraseña")
         String verifyPassword,
-        Role role,
+
+        @NotBlank(message = "El nombre no puede estar vacío")
         String nombre,
+
+        @NotBlank(message = "El apellido no puede estar vacío")
         String apellido,
+
+        @NotBlank(message = "El puesto no puede estar vacío")
         String puesto
-) {
-}
+) {}
