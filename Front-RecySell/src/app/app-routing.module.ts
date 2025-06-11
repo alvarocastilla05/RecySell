94568b341886d9c0f4981135351adc042e69b4b5
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2025-06-11 14:38:40 +0200

    Inicio de sesión y configuracion de cors

--- Front-RecySell/src/app/app-routing.module.ts
+++ Front-RecySell/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: InicioSesionComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'registro', component: RegistroComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-RecySell/src/app/components/inicio-sesion/inicio-sesion.component.ts
+++ Front-RecySell/src/app/components/inicio-sesion/inicio-sesion.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthServiceService } from '../../service/auth-service.service';
+import { Router } from '@angular/router'; // <-- Importa Router
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrl: './inicio-sesion.component.css'
 })
 export class InicioSesionComponent {
+  loginForm: FormGroup;
+  errorMsg: string = '';
+  showPassword = false;
 
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthServiceService,
+    private router: Router // <-- Inyecta Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    const { username, password } = this.loginForm.value;
+
+    this.authService.login(username, password).subscribe({
+      next: (res) => {
+        // Redirige a /home si el login es exitoso
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        this.errorMsg = 'Usuario o contraseña incorrectos';
+      }
+    });
+  }
 }
